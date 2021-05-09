5eb5876381e76393e284ae6c42be2e7c1cee28d7
Dermot McEneaney <dermot.mceneaney@protonmail.com>
2021-05-09 16:41:00 +0100

    added Ray struct

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,30 @@ const IMAGE_HEIGHT: u16 = 256;
 type Colour = Vec3;
 type Point3 = Vec3;
 
+struct Ray {
+    origin: Point3,
+    direction: Vec3
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self{origin, direction}
+    }
+
+    pub fn origin(&self) -> Point3 {self.origin}
+ 
+    pub fn direction(&self) -> Vec3 {self.direction}
+
+    pub fn at(&self, t: f32) -> Point3 {
+        self.origin + self.direction * t
+    }
+}
+
 fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_WIDTH);
 
     for j in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\nScanlines remaining: {}", j);
+        eprint!("Scanlines remaining: {}\n", j);
         for i in 0..IMAGE_WIDTH {
             let pixel_colour = Colour::new(
                 (i as f32 / (IMAGE_WIDTH - 1) as f32) * 256.0,
@@ -19,4 +38,7 @@ fn main() {
             pixel_colour.print_u8();
         }
     }
+
+    let ray = Ray::new(Point3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 1.0, 1.0));
+    eprintln!("{}", ray.at(0.1));
 }
