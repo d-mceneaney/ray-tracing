d3348628562689da2d3b414b6de7154d17d7d737
Dermot McEneaney <dermot.mceneaney@protonmail.com>
2021-05-09 18:17:43 +0100

    added Ray struct and fixed ppm image header typo

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,13 @@
 use vec3::Vec3;
+use ray::Ray;
 
-const IMAGE_WIDTH: u16 = 256;
-const IMAGE_HEIGHT: u16 = 256;
+const IMAGE_WIDTH: u16 = 1920;
+const IMAGE_HEIGHT: u16 = 1080;
 type Colour = Vec3;
 type Point3 = Vec3;
 
-struct Ray {
-    origin: Point3,
-    direction: Vec3
-}
-
-impl Ray {
-    pub fn new(origin: Point3, direction: Vec3) -> Self {
-        Self{origin, direction}
-    }
-
-    pub fn origin(&self) -> Point3 {self.origin}
- 
-    pub fn direction(&self) -> Vec3 {self.direction}
-
-    pub fn at(&self, t: f32) -> Point3 {
-        self.origin + self.direction * t
-    }
-}
-
 fn main() {
-    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_WIDTH);
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("Scanlines remaining: {}\n", j);
