28bceb30cf920587606f82b2b680af6bd966f3a1
Dermot McEneaney <dermot.mceneaney@protonmail.com>
2021-05-09 16:27:49 +0100

    added Vec3 lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use vec3::Vec3;
+
 const IMAGE_WIDTH: u16 = 256;
 const IMAGE_HEIGHT: u16 = 256;
+type Colour = Vec3;
+type Point3 = Vec3;
 
 fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_WIDTH);
@@ -7,14 +11,12 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\nScanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let red = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let green = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let blue = (0.25 * 256.0) as u8;
-
-            let red = (red * 256.0) as u8;
-            let green = (green * 256.0) as u8;
-
-            print!("{} {} {}\n", red, green, blue);
+            let pixel_colour = Colour::new(
+                (i as f32 / (IMAGE_WIDTH - 1) as f32) * 256.0,
+                (j as f32 / (IMAGE_HEIGHT - 1) as f32) * 256.0,
+                0.25 * 256.0,
+            );
+            pixel_colour.print_u8();
         }
     }
 }
