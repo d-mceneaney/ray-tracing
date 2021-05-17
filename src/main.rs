ea8a2126354e5ef65a6ebbcbb0feab25d7869319
Dermot McEneaney <dermot.mceneaney@intel.com>
2021-05-17 20:51:47 +0100

    added max depth to prevent stack overflow due to recursive ray function

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,21 @@ const IMAGE_WIDTH: u16 = 400;
 const IMAGE_HEIGHT: u16 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u16;
 const FOCAL_LENGTH: f32 = 1.0;
 const SAMPLES_PER_PIXEL: u8 = 100;
+const MAX_DEPTH: u8 = 50;
 type Colour = Vec3;
 type Point3 = Vec3;
 
-fn ray_colour(ray: &Ray, world: &HittableList) -> Colour {
+fn ray_colour(ray: &Ray, world: &HittableList, depth: u8) -> Colour {
+    if depth <=0 {
+        return Colour::new_i32(0, 0, 0);
+    }
+
     let mut record = HitRecord::new();
 
         if world.hit(ray, 0.0, f32::INFINITY, &mut record) {
-            //let target: Point3 = record.p + record.normal + random_vec3_in_unit_sphere();
-            //return ray_colour(&Ray::new(record.p, target - record.p), &world) * 0.5;
-            return (Colour::new_i32(1, 1, 1) + record.normal) * 0.5;
+            let target: Point3 = record.p + record.normal + random_vec3_in_unit_sphere();
+            return ray_colour(&Ray::new(record.p, target - record.p), &world, depth - 1) * 0.5;
+           // return (Colour::new_i32(1, 1, 1) + record.normal) * 0.5;
     }
     let unit_direction = ray.direction().unit_vec();
     let t = (unit_direction.y() + 1.0) * 0.5;
@@ -51,7 +56,7 @@ fn main() {
                 let u = (i as f32 + rng.gen_range(0.0..1.0)) / (IMAGE_WIDTH - 1) as f32;
                 let v = (j as f32 + rng.gen_range(0.0..1.0)) / (IMAGE_HEIGHT - 1) as f32;
                 let ray = camera.get_ray(u, v);
-                pixel_colour = pixel_colour + ray_colour(&ray, &world);
+                pixel_colour = pixel_colour + ray_colour(&ray, &world, MAX_DEPTH);
             }
             write_color(pixel_colour, SAMPLES_PER_PIXEL);
         }
